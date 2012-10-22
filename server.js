3e16fe228e26236a40b81c09dbb5017b99cfcac0
Nathan Friedly <nathan@nfriedly.com>
2012-10-22 11:31:26 -0700

    added pretty home page, moved api to /text

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
+#!/usr/bin/env node
+
 var cluster = require('cluster');
 var http = require('http');
 var numCPUs = require('os').cpus().length;
+var fs = require('fs');
+var URL = require('url');
 
 if (!process.env.PORT) {
 	console.error('The PORT environment variable must be set');
@@ -16,13 +20,24 @@ if (cluster.isMaster) {
 		console.log('worker ' + worker.process.pid + ' died');
 	});
 } else {
+
+	var template = fs.readFileSync('index.html').toString();
+
 	http.createServer(function(req, res) {
-		if (req.headers['x-forwarded-for']) {
-			res.writeHead(200);
-			res.end(req.headers['x-forwarded-for']);
-		} else {
+		var ip = req.headers['x-forwarded-for']; 
+		var path = URL.parse(req.url).pathname;
+		if (!ip) {
 			res.writeHead(500);
 			res.end('Unable to determine your IP. This script expects a load balancer, such as the one that heroku.com provides, to set the x-forwarded-for header.');
+		} else if (path == "/") {
+			res.writeHead(200);
+			res.end(template.replace(/\{ip\}/g, ip).replace(/\{domain\}/g, req.headers['host']));
+		} else if (path == '/text') {
+			res.writeHead(200);
+			res.end(ip);
+		} else {
+			res.writeHead(404);
+			res.end('404 file not found');
 		}
 	}).listen(process.env.PORT);
 }
